910cfc49a6d4ac131b8258c72f39ecab2d606bbb
dan_gwn <danielgawne@kubrickgroup.com>
2021-01-22 12:11:09 +0000

    create data docs

--- model_code/create_data.py
+++ model_code/create_data.py
@@ -15,7 +15,17 @@ data_dir = file_paths['data_dir'][0]
 
 review_data = pd.read_csv(os.path.join(data_dir, 'reviews.csv'))
 
-def create_data(final_doc_len = 200):
+def create_data(final_doc_len = 100):
+    '''
+    Creates the features and labels for the imdb data
+    =============================================
+    Inputs:
+      - final_doc_len: the number of characters in our array of vectors
+        - default: 100
+    Returns:
+      - X: A numpy array of the features
+      - y: A numpy array of the labels
+    '''
     features = []
     labels = []
 
@@ -29,9 +39,5 @@ def create_data(final_doc_len = 200):
     y = np.array(labels)
 
     return X, y
-
-if __name__ == '__main__':
-    X,y = create_data(final_doc_len = 140)
-    print(X[0], y[0])
 
 
