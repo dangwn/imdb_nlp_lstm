f37432872910529877bbc856ab9c9166251288c9
dan_gwn <danielgawne@kubrickgroup.com>
2021-01-22 12:27:04 +0000

    model docs

--- model_code/models.py
+++ model_code/models.py
@@ -8,12 +8,40 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class lstm(nn.Module):
+    '''
+    A PyTorch lstm model for predicting positive/negative sentiments in documents
+    =============================================
+    Model Input:
+      - A torch tensor of size (batch size, sequence length, number of features)
+    Model Output:
+      - An (non-softmaxed) tensor of size (batch_size, number of classes)
+    =============================================
+    Attributes:
+      - lstm_blocks  : The number of lstm blocks used
+      - lstm_channels: The number of features in the hidden layers of each lstm block
+      - hidden_layers: The number of extra hidden layers
+      - lstm         : The model lstm layer
+      - fc_in        : The connector between the lstm and hidden layers
+      - fc_hidden    : The connectors between the hidden layers
+      - fc_out       : The connector between the hidden layers and output layer
+    '''
     def __init__(
         self,
         in_channels, out_channels,
         lstm_blocks = 1, lstm_channels = 64,
         hidden_layers = 1, hidden_channels = 64,
     ):
+        '''
+        Class initialization
+        =========================================
+        Inputs:
+          - in_channels    : The number of features in each sequence
+          - out_channels   : The number of output channels
+          - lstm_blocks    : The number of lstm blocks within the lstm layer
+          - lstm_channels  : The number of features in the hidden layer of each lstm block
+          - hidden_layers  : The number of extra hidden layers
+          - hidden_channels: The number of features in the hidden layers
+        '''
         super().__init__()
 
         assert lstm_blocks > 0 and hidden_layers >= 0
@@ -30,6 +58,14 @@ class lstm(nn.Module):
         self.fc_out = nn.Linear(hidden_channels, out_channels)
 
     def forward(self, x):
+        '''
+        Forward method
+        =========================================
+        Inputs:
+          - x: The tensor to be passed through the model of the shape (batch size, sequence length, number of features)
+        Outputs:
+          - A torch tensor of the output of the model, of the form (batch_size, number of outputs)
+        '''
         h0 = torch.zeros(self.lstm_blocks, x.size(0), self.lstm_channels).to(x.device)
         c0 = torch.zeros(self.lstm_blocks, x.size(0), self.lstm_channels).to(x.device)
         
